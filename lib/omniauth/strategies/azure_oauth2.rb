62543939f57e7b3ea36553bf4cf9271f239636fe
Ben Campbell <ben@objectiveinc.com>
2018-10-11 16:07:07 -0600

    Verify JWT token
    - Add nonce and verify on callback phase
    - Verify `aud` (audience) jwt claim. It should be the same as your
    client id.
    - Explicitly pass verify options to JWT.decode, including expiration,
    not before, iat.
    - Not verifying iss because it can be a different value depending on the
    type of account that's being logged in. For an Azure AD account, I'm not
    sure yet how to verify this value or if it should even *be* verfied. The
    docs don't mention anything about verifying this value; they simply
    state that you can figure out if the person used a personal account by
    matching up the iss value to a constant value that Microsoft uses to
    identify that type of account.
    - Add rubocop.yml file. I might tweak these options. Mainly didn't want
    warnings for line length over 80.
    
    Still to do:
    
    - Add new tests where needed and adapt tests from the other two
      repositores and change as needed.
    - Update Docs
    
    Also, this isn't really tested for a client that is not using the
    'common' endpoint. I probably won't be spending any time on that part.

--- lib/omniauth/strategies/azure_oauth2.rb
+++ lib/omniauth/strategies/azure_oauth2.rb
@@ -1,58 +1,65 @@
-require 'omniauth/strategies/oauth2'
+#-------------------------------------------------------------------------------
+# Copyright (c) 2015 Micorosft Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+# Attribution:
+# Most of this code is patched together from the following repositories:
+#
+# https://github.com/AzureAD/omniauth-azure-activedirectory/
+# https://github.com/KonaTeam/omniauth-azure-oauth2
+
 require 'jwt'
+require 'omniauth/strategies/oauth2'
+require 'openssl'
+require 'securerandom'
 
 module OmniAuth
   module Strategies
+    # Main class for Azure OAuth2 strategy.
     class AzureOauth2 < OmniAuth::Strategies::OAuth2
       BASE_AZURE_URL = 'https://login.microsoftonline.com'
+      BASE_SCOPES = %w[openid profile email].freeze
 
       option :name, 'azure_oauth2'
-
-      option :tenant_provider, nil
-
-      # AD resource identifier
-      # option :resource, '00000002-0000-0000-c000-000000000000'
-
-      # tenant_provider must return client_id, client_secret and optionally tenant_id and base_azure_url
-      args [:tenant_provider]
+      option :tenant, 'common'
+      option :client_id, nil
+      option :client_secret, nil
+      option :scope, BASE_SCOPES.join(' ')
 
       def client
-        if options.tenant_provider
-          provider = options.tenant_provider.new(self)
-        else
-          provider = options  # if pass has to config, get mapped right on to options
-        end
+        tenant = options.tenant
+        options.client_options.authorize_url = "#{BASE_AZURE_URL}/#{tenant}/oauth2/v2.0/authorize"
+        options.client_options.token_url = "#{BASE_AZURE_URL}/#{tenant}/oauth2/v2.0/token"
 
-        options.client_id = provider.client_id
-        options.client_secret = provider.client_secret
-        options.tenant_id =
-          provider.respond_to?(:tenant_id) ? provider.tenant_id : 'common'
-        options.base_azure_url =
-          provider.respond_to?(:base_azure_url) ? provider.base_azure_url : BASE_AZURE_URL
-
-        options.authorize_params.domain_hint = provider.domain_hint if provider.respond_to?(:domain_hint) && provider.domain_hint
-        options.authorize_params.prompt = request.params['prompt'] if request.params['prompt']
-        options.authorize_params.scope = 'openid email profile'
-        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/authorize"
-        options.client_options.token_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/token"
-
-        # options.token_params.resource = options.resource
         super
       end
 
-      uid {
-        raw_info['sub']
-      }
+      uid { claims['sub'] }
 
       info do
         {
-          name: raw_info['name'],
-          nickname: raw_info['unique_name'],
-          first_name: raw_info['given_name'],
-          last_name: raw_info['family_name'],
-          email: raw_info['email'] || raw_info['upn'],
-          oid: raw_info['oid'],
-          tid: raw_info['tid']
+          name: claims['name'] || claims['oid'],
+          email: claims['email'] || claims['preferred_username'],
+          oid: claims['oid'],
+          tid: claims['tid']
         }
       end
 
@@ -60,11 +67,120 @@ module OmniAuth
         full_host + script_name + callback_path
       end
 
-      def raw_info
-        # it's all here in JWT http://msdn.microsoft.com/en-us/library/azure/dn195587.aspx
-        @raw_info ||= ::JWT.decode(access_token.params["id_token"], nil, false).first
+      def authorize_params
+        options.authorize_params[:nonce] = new_nonce
+
+        super
       end
 
+      def claims
+        @id_token ||= access_token.params['id_token']
+        @claims ||= validate_and_parse_id_token(@id_token)
+      end
+
+      private
+
+      def validate_and_parse_id_token(id_token)
+        jwt_claims =
+          JWT.decode(id_token, nil, true, verify_options) do |header|
+            # There should always be one key from the discovery endpoint that
+            # matches the id in the JWT header.
+            x5c = (signing_keys.find do |key|
+              key['kid'] == header['kid']
+            end || {})['x5c']
+            if x5c.nil? || x5c.empty?
+              raise JWT::VerificationError, 'No keys from key endpoint match the id token'
+            end
+            # The key also contains other fields, such as n and e, that are
+            # redundant. x5c is sufficient to verify the id token.
+            OpenSSL::X509::Certificate.new(JWT.base64url_decode(x5c.first)).public_key
+          end
+
+        claims = jwt_claims.first
+
+        return claims if claims['nonce'] == read_nonce
+
+        raise JWT::DecodeError, 'Returned nonce did not match.'
+      end
+
+      ##
+      # The keys used to sign the id token JWTs. This is just a memoized version
+      # of #fetch_signing_keys.
+      #
+      # @return Array[Hash]
+      def signing_keys
+        @signing_keys ||= fetch_signing_keys
+      end
+
+      ##
+      # Fetches the current signing keys for Azure AD. Note that there should
+      # always two available, and that they have a 6 week rollover.
+      #
+      # Each key is a hash with the following fields:
+      #   kty, use, kid, x5t, n, e, x5c
+      #
+      # @return Array[Hash]
+      def fetch_signing_keys
+        response = JSON.parse(Net::HTTP.get(URI(signing_keys_url)))
+        response['keys']
+      rescue JSON::ParserError
+        raise StandardError, 'Unable to fetch AzureAD signing keys.'
+      end
+
+      ##
+      # The location of the public keys of the token signer. This is parsed from
+      # the OpenId config response.
+      #
+      # @return String
+      def signing_keys_url
+        return openid_config['jwks_uri'] if openid_config.include? 'jwks_uri'
+        raise StandardError, 'No jwks_uri in OpenId config response.'
+      end
+
+      #
+      # A memoized version of #fetch_openid_config.
+      #
+      # @return Hash
+      def openid_config
+        @openid_config ||= fetch_openid_config
+      end
+
+      def fetch_openid_config
+        JSON.parse(Net::HTTP.get(URI(openid_config_url)))
+      rescue JSON::ParserError
+        raise StandardError, 'Unable to fetch OpenId configuration for ' \
+          'AzureAD tenant.'
+      end
+
+      def openid_config_url
+        "https://login.microsoftonline.com/#{options.tenant}/v2.0/.well-known/openid-configuration/"
+      end
+
+      def new_nonce
+        session['azure_oauth2.nonce'] = SecureRandom.uuid
+      end
+
+      def read_nonce
+        session.delete 'azure_oauth2.nonce'
+      end
+
+      ##
+      # The options passed to the Ruby JWT library to verify the id token.
+      # Note that these are not all the checks we perform. Some (like nonce)
+      # are not handled by the JWT API and are checked manually in
+      # #validate_and_parse_id_token.
+      #
+      # https://github.com/jwt/ruby-jwt#support-for-reserved-claim-names
+      #
+      # @return Hash
+      def verify_options
+        { verify_expiration: true,
+          verify_not_before: true,
+          verify_iat: true,
+          verify_iss: false,
+          verify_aud: true,
+          'aud' => client.id }
+      end
     end
   end
 end
